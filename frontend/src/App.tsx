b6db22e274bc3202afc401e4b58c1e71b5c5b3a5
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-02-10 01:06:05 +0900

    add : ゲージとチャートの関数化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,6 +35,36 @@ function Footer() {
     );
 }
 
+function RadialGaugeComponent() {
+    return (
+        <IgrRadialGauge
+            height="300px"
+            width="300px"
+            minimumValue={0}
+            maximumValue={100}
+            value={50}
+            interval={10}
+            needleBrush="DodgerBlue"
+            scaleBrush="LightGray"
+            backingBrush="White"
+            backingOutline="LightGray"
+        />
+    );
+}
+
+function LineChartComponent({ data }: { data: never[] }) {
+    return (
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="amt" stroke="#ffc658" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </LineChart>
+    );
+}
+
 const App = () => {
     const [data, setData] = useState([]);
     const [rawData, setRawData] = useState('');
@@ -48,34 +78,12 @@ const App = () => {
             });
     }, []);
 
-    const renderLineChart = (
-        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="amt" stroke="#ffc658" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="name" />
-            <YAxis />
-        </LineChart>
-    );
-
     return (
         <div>
             <Header />
             <Container>
-                <IgrRadialGauge
-                    height="300px"
-                    width="300px"
-                    minimumValue={0}
-                    maximumValue={100}
-                    value={50}
-                    interval={10}
-                    needleBrush="DodgerBlue"
-                    scaleBrush="LightGray"
-                    backingBrush="White"
-                    backingOutline="LightGray"
-                />
-                {renderLineChart}
+                <RadialGaugeComponent />
+                <LineChartComponent data={data} />
                 <Button variant="contained">Hello world</Button>
                 <pre>{rawData}</pre>
             </Container>
