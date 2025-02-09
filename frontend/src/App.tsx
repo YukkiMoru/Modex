55b3a78634832d45c58f5bd174e6ad3d6887086e
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-02-10 00:53:32 +0900

    add : ヘッダーとフッターの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,11 @@ import './App.css';
 import { IgrRadialGauge, IgrRadialGaugeModule } from 'igniteui-react-gauges';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 IgrRadialGaugeModule.register();
 
@@ -32,22 +37,38 @@ const App = () => {
 
     return (
         <div>
-            <h1>こんにちは</h1>
-            <IgrRadialGauge
-                height="300px"
-                width="300px"
-                minimumValue={0}
-                maximumValue={100}
-                value={50}
-                interval={10}
-                needleBrush="DodgerBlue"
-                scaleBrush="LightGray"
-                backingBrush="White"
-                backingOutline="LightGray"
-            />
-            {renderLineChart}
-            <Button variant="contained">Hello world</Button>
-            <pre>{rawData}</pre>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        ウェブサイトのタイトル
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <h1>こんにちは</h1>
+                <IgrRadialGauge
+                    height="300px"
+                    width="300px"
+                    minimumValue={0}
+                    maximumValue={100}
+                    value={50}
+                    interval={10}
+                    needleBrush="DodgerBlue"
+                    scaleBrush="LightGray"
+                    backingBrush="White"
+                    backingOutline="LightGray"
+                />
+                {renderLineChart}
+                <Button variant="contained">Hello world</Button>
+                <pre>{rawData}</pre>
+            </Container>
+            <Box mt={5}>
+                <Typography variant="body2" color="textSecondary" align="center">
+                    {'Copyright © '}
+                    <a href="https://github.com/YukkiMoru/Modex" target="_blank" rel="noopener noreferrer">Modex</a> {new Date().getFullYear()}
+                    {'.'}
+                </Typography>
+            </Box>
         </div>
     );
 }
