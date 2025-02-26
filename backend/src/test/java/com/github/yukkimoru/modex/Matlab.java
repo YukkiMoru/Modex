6b7ec9a1d2f426558f9870043d99f0ba6f3e4977
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-02-26 21:02:42 +0900

    add : implement MATLAB function call to compute square roots

--- backend/src/test/java/com/github/yukkimoru/modex/Matlab.java
+++ backend/src/test/java/com/github/yukkimoru/modex/Matlab.java
@@ -3,8 +3,13 @@ package com.github.yukkimoru.modex;
 import com.mathworks.engine.*;
 
 public class Matlab {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         MatlabEngine eng = MatlabEngine.startMatlab();
+        double[] a = {2.0 ,4.0, 6.0};
+        double[] roots = eng.feval("sqrt", a);
+        for (double e: roots) {
+            System.out.println(e);
+        }
         eng.close();
     }
 }
